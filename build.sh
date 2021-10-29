a6fd7f94682f36387b78099abf6dd869403b930a
Glitch (unblocknet) <none>
2021-10-29 06:25:27 +0000

    Export from Glitch

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
-#!/bin/sh
+#unzip命令被取消，无法自动更新，需要手动更新
 
-#mkdir
-mkdir /app/usr
-mkdir /app/usr/bin
+#define folders
+CONFIG="/app/etc/v2ray"
+APP="/app/usr/bin"
 
-# Global variables
-DIR_CONFIG="/app/etc/v2ray"
-DIR_RUNTIME="/app/usr/bin"
-DIR_TMP="$(mktemp -d)"
-
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/v2ray.json
+#write config.json
+cat << EOF > ${CONFIG}/config.json
 {
     "inbounds": [{
         "port": $PORT,
@@ -34,18 +29,8 @@ cat << EOF > ${DIR_TMP}/v2ray.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+#configure
+${APP}/v2ctl config ${CONFIG}/config.json > ${CONFIG}/config.pb
 
-# Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/v2ray.json > ${DIR_CONFIG}/config.pb
-
-# Install V2Ray
-mv ${DIR_TMP}/v2ray ${DIR_RUNTIME}/v2ray
-chmod 755 ${DIR_RUNTIME}/v2ray
-rm -rf ${DIR_TMP}
-
-# Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+#run
+${APP}/v2ray -config=${CONFIG}/config.pb
