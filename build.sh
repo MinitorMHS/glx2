86648b7b006377b0982b763acbaf1744fb4c2bfc
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-10-29 10:48:07 +0800

    glitch无法使用unzip，此法失效

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ cat << EOF > ${DIR_TMP}/v2ray.json
 }
 EOF
 
-# Get V2Ray executable release 
+# Get V2Ray executable release
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
